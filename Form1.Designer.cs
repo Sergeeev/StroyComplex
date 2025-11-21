991bc70bb664fb09d7edba28e91a45386070d325
Sergeeev <156417572+Sergeeev@users.noreply.github.com>
2025-11-21 10:04:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Проекты с задачами и сотрудниками",
+            "Задачи проекта 1",
+            "Количество задач у сотрудников",
+            "Материалы дороже 50 руб",
+            "Задачи в работе",
+            "Все проекты и задачи",
+            "Сотрудники без задач"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(330, 12);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(100, 23);
+            this.btnLoadData.TabIndex = 2;
+            this.btnLoadData.Text = "Загрузить";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "База данных СтройКомплекс";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnLoadData;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Server=127.0.0.1;Database=СтройКомплекс;Uid=root;Pwd=;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "";
+
+                if (comboBox1.SelectedIndex == 0)
+                    query = "SELECT Проекты.НазваниеПроекта, Задачи.НазваниеЗадачи, Сотрудники.Имя, Сотрудники.Фамилия FROM Задачи JOIN Проекты ON Задачи.ПроектID = Проекты.ПроектID JOIN Сотрудники ON Задачи.Назначена = Сотрудники.СотрудникID";
+                else if (comboBox1.SelectedIndex == 1)
+                    query = "SELECT Задачи.НазваниеЗадачи, Задачи.Статус, Сотрудники.Имя, Сотрудники.Фамилия FROM Задачи JOIN Сотрудники ON Задачи.Назначена = Сотрудники.СотрудникID WHERE Задачи.ПроектID = 1";
+                else if (comboBox1.SelectedIndex == 2)
+                    query = "SELECT Сотрудники.Имя, Сотрудники.Фамилия, COUNT(Задачи.ЗадачаID) AS КоличествоЗадач FROM Сотрудники LEFT JOIN Задачи ON Сотрудники.СотрудникID = Задачи.Назначена GROUP BY Сотрудники.СотрудникID";
+                else if (comboBox1.SelectedIndex == 3)
+                    query = "SELECT * FROM Материалы WHERE ЦенаЗаЕдиницу > 50";
+                else if (comboBox1.SelectedIndex == 4)
+                    query = "SELECT Задачи.НазваниеЗадачи, Проекты.НазваниеПроекта FROM Задачи JOIN Проекты ON Задачи.ПроектID = Проекты.ПроектID WHERE Задачи.Статус = 'в работе'";
+                else if (comboBox1.SelectedIndex == 5)
+                    query = "SELECT Проекты.НазваниеПроекта, Задачи.НазваниеЗадачи FROM Проекты LEFT JOIN Задачи ON Проекты.ПроектID = Задачи.ПроектID";
+                else if (comboBox1.SelectedIndex == 6)
+                    query = "SELECT Сотрудники.Имя, Сотрудники.Фамилия FROM Сотрудники LEFT JOIN Задачи ON Сотрудники.СотрудникID = Задачи.Назначена WHERE Задачи.ЗадачаID IS NULL";
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
